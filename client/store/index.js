cd20f35ff7674520820e7d903e10bc2541c56b7b
Dylan <dylanhrebenach@gmail.com>
2019-01-08 15:19:06 -0800

    f LandingPage17

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,10 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import userReducer from './user'
+import wineReducer from './wine'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({userReducer, wineReducer})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
---
+++ client/store/wine.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import history from '../history'
+import {runInNewContext} from 'vm'
+
+// ACTION TYPES
+const GET_FEATURED_WINES = 'GET_FEATURED_WINES'
+
+// ACTION CREATORS
+
+const getFeaturedWines = wines => ({
+  type: GET_FEATURED_WINES,
+  wines
+})
+
+// THUNK CREATORS
+export const thunk_gotFeaturedWines = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/wines')
+    dispatch(getFeaturedWines(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// INITIAL STATE
+const initialState = {
+  featuredWines: []
+}
+
+// WINE SUB-REDUCER
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_FEATURED_WINES:
+      return {...state, featuredWines: action.wines}
+    default:
+      return state
+  }
+}
