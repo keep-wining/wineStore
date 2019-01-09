ba6c3f33f92c5001836739dd965ba805d440558e
Victor Marta <vicm159@gmail.com>
2019-01-09 12:00:09 -0800

    Finish NewAccount22

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import {auth, authNewAccount} from '../store'
 
 /**
  * COMPONENT
@@ -23,6 +23,75 @@ const AuthForm = props => {
             <small>Password</small>
           </label>
           <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+      <a href="/auth/google">{displayName} with Google</a>
+    </div>
+  )
+}
+
+const NewAccount = props => {
+  const {name, displayName, handleSubmit, error} = props
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="zip">
+            <small>Zip Code</small>
+          </label>
+          <input name="zip" type="number" />
+        </div>
+        <div>
+          <label htmlFor="city">
+            <small>City</small>
+          </label>
+          <input name="city" type="text" />
+        </div>
+        <div>
+          <label htmlFor="address1">
+            <small>Address 1</small>
+          </label>
+          <input name="address1" type="text" />
+        </div>
+        <div>
+          <label htmlFor="address2">
+            <small>Address 2</small>
+          </label>
+          <input name="address2" type="text" />
+        </div>
+        <div>
+          <label htmlFor="state">
+            <small>State</small>
+          </label>
+          <input name="state" type="text" />
         </div>
         <div>
           <button type="submit">{displayName}</button>
@@ -69,8 +138,40 @@ const mapDispatch = dispatch => {
   }
 }
 
+const mapDispatchNewAccount = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      const firstName = evt.target.firstName.value
+      const lastName = evt.target.lastName.value
+      const zip = evt.target.zip.value
+      const city = evt.target.city.value
+      const address1 = evt.target.address1.value
+      const address2 = evt.target.address2.value
+      const state = evt.target.state.value
+      dispatch(
+        authNewAccount({
+          email,
+          password,
+          formName,
+          firstName,
+          lastName,
+          zip,
+          city,
+          address1,
+          address2,
+          state
+        })
+      )
+    }
+  }
+}
+
 export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
+export const Signup = connect(mapSignup, mapDispatchNewAccount)(NewAccount)
 
 /**
  * PROP TYPES
--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
+import FeaturedWines from '../client/components/FeaturedWines'
 
 /**
  * COMPONENT
@@ -21,14 +22,16 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/" component={FeaturedWines} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
-        {/* Displays our Login component as a fallback */}
+        {/* Displays our Login component as a fallback
         <Route component={Login} />
+        */}
       </Switch>
     )
   }
