63c282d31b8edde2b0162e250ab05f4b455e649c
Kristi Kelly <kristikelly2001@yahoo.com>
2019-01-11 19:22:51 -0800

    RESUBMIT  in progress test specs #48

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,13 +1,17 @@
 const db = require('../db')
 const Sequelize = require('sequelize')
 
-const Order = db.define('orders', {
-  details: {
-    type: Sequelize.STRING
-  },
-  price: {
-    type: Sequelize.INTEGER
-  }
-})
+const Order = db.define(
+  'orders',
+  {
+    details: {
+      type: Sequelize.STRING
+    },
+    price: {
+      type: Sequelize.INTEGER
+    }
+  }, // comment the schema: private line below to test
+  {schema: 'private'}
+)
 
 module.exports = Order
